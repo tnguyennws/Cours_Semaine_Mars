7b4e2b4806ecf6dd061908be545c25796c42933a
Timothée <tnguyen@normandiewebschool.fr>
2022-03-14 11:52:27 +0100

    📦 NEW: Ajout de la fonction pour trouver si le 1er janvier est un dimanche

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function findCircleArea(radius) {
-  const pi = 3.14159;
+  const pi = Math.PI;
   const area = radius * pi * pi;
+
+  console.log(area);
 
   return area;
 }
+
+function whenIsFirstOfJanuary() {
+  for (let year = 2014; year < 2050; ++year) {
+    let day = new Date(year, 0, 1);
+    if (day.getDay() === 0) {
+      console.log("The 1st January of " + year + " is a Sunday");
+    }
+  }
+}
+
+findCircleArea(3);
+whenIsFirstOfJanuary();
